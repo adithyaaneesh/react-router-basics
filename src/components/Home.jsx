002afb25a855d1884a58212b08c49ff72eca1003
adithyaaneesh <adithyaaneesh29@gmail.com>
2026-04-19 20:34:37 +0530

    react-router-dom basics day1

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
+const Home = () => {
+
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <>
+    <div
+      style={{
+        background: theme === "light" ? "#fff" : "#333",
+        color: theme === "light" ? "#000" : "#fff",
+        height: "100vh",
+        textAlign: "center"
+      }}>
+        <h1>This is the home page </h1>
+      <h2>{theme.toUpperCase()} MODE</h2>
+
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+    </>
+    
+  );
+};
+
+export default Home;
